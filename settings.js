ee92e769a4261b0d1c9e8b9c315c2e548fa72c2d
MannNoMercy <125950848+MannNoMercy@users.noreply.github.com>
2025-05-24 00:18:54 +0800

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,244 @@
+// RECODE BY Zynz
+/////////////////
+const chalk = require("chalk")
+const fs = require("fs")
+//aumto presence update
+global.autoTyping = false //auto tying in gc (true to on, false to off)
+global.autoRecord = false //auto recording (true to on, false to off)
+global.autoblockmorroco = true //auto block 212 (true to on, false to off)
+global.wlcm = false
+global.autokickmorroco = true //auto kick 212 (true to on, false to off) 
+global.antispam = false//auto kick spammer (true to on, false to off)
+//////////////////////////////////////////////////////////////////////////////////
+//BATAS//
+//=========UBAH BAGIAN THUMBNAIL MENU & ALLMENU==========//
+global.gif = fs.readFileSync('./data/image/yoimiya.mp4'), // BISA JUGA DI BAGIAN DATA/IMAGE/YOIMIYA.MP4
+global.thumbnail = 'https://files.catbox.moe/6an8o0.jpg', //THUMB MENU KALIAN
+global.musikmenu = 'https://files.catbox.moe/6an8o0.jpg' //THUMB MUSIK KALIAN
+global.dinzmenu = 'https://files.catbox.moe/6an8o0.jpg', //THUMB MENU button KALIAN
+/////////////////////////////////////////////////////////////////////////////////
+//BATAS//
+//////////////////////SETTING TAMPILAN MENU KALIAN//////////////////
+global.ig = 'GAK ADA' //NAMA IG LU
+global.yt = 'Elyssiaカンティ✨' //NAMA YT LU, KALO GADA GAUSAH DIISI
+global.ttowner = 'Aliya' //NAMA TIKTOK LU
+global.ownername = '✨️ Elyssiaカンティ✨ ✨️' //NAMA LU
+global.owner = ['6285753124507','6285753124507@s.whatsapp.net'] // SETTING JUGA DI FOLDER DATABASE 
+global.ownernomer = '6285753124507' // NOMOR LU
+global.socialm = 'GitHub: -'
+global.location = 'Indonesia' 
+global.nameCreator = 'ZynzOfc | RorwOfc'
+/////////////////////////////////////////////////////////////////////////////////
+//BATAS//
+//=================SETTING PAYMENT KALIAN===================\\
+global.nodana = '6285251913873' // KOSONG KAN JIKA TIDAK ADA
+global.nogopay = '6285251913873' // KOSONG KAN JIKA TIDAK ADA 
+global.noovo = '6285251913873' // KOSONG KAN JIKA TIDAK ADA
+/////////////////////////////////////////////////////////////////////////////////
+//BATAS//
+//==============SETTING PAYMENT NAME=======================\\
+global.andana = '' // KOSONG KAN JIKA TIDAK ADA
+global.angopay = '' // KOSONG KAN JIKA TIDAK ADA
+global.anovo = '' // KOSONG KAN JIKA TIDAK ADA
+//////////////////////////////////////////////////////////////////////////////////
+//BATAS//
+//==================SETTING BOT===========================\\
+global.botname = "Cinamorrol-ᴀɪ" //NAMA BOT LU
+global.ownernumber = '6285753124507' //NOMOR LU
+global.botnumber = '6285753124507' //NOMOR LU
+global.ownername = 'Elyssiaカンティ✨ Ofc' //NAMA LU
+global.idSaluran = "120363283540203585@newsletter"//ID SALURAN LU
+global.idch = "120363283540203585@newsletter"//ID SALURAN LU
+global.namaSaluran = "Zynz Rorw | kazeo" //NAMA SALURAN LU
+global.linkSaluran = "https://whatsapp.com/channel/0029Vb7kHwz8Pgs9MiuRVN2a"//NAMA SALURAN LU
+global.ownerNumber = ["6285753124507@s.whatsapp.net"] //NOMORLU
+global.ownerweb = ""//WEB LU//OPSIONAL
+global.websitex = ""//OPSIONAL
+global.wagc = "gakada"//GRUP LU
+global.saluran = "https://whatsapp.com/channel/0029Vaxdmyd3bbV7IWreRt34"//SALURAN LU
+global.themeemoji = '🪀'
+global.wm = "Elyssiaカンティ✨ | Cinamorrol-ᴀɪ"//WM LU
+global.botscript = '.owner' //script link//OPSIONAL
+global.packname = "Elyssiaカンティ✨ Ofc"//PACKNAME BEBAS 
+global.author = "\n\nCreate by Elyssiaカンティ✨ Ofc|Cinamorrol-ᴀɪ\n Dev : Elyssiaカンティ✨ || Cinamorrol"//AUTHOR BEBAS
+global.creator = "6285753124507@s.whatsapp.net" // NOMOR LU
+/////////////////////////////////////////////////////////////////////////////////
+//BATAS//
+//================== CPANEL FITUR ==========================\\
+global.domain = '-' // Isi Domain Lu jangan kasih tanda / di akhir link
+global.apikey = '-' // Isi Apikey Plta Lu
+global.capikey = '-' // Isi Apikey Pltc Lu
+/////////////////////////////////////////////////////////////////////////////////
+//BATAS//
+///////////////////Server create panel egg pm2 ///////////////////////
+global.apikey2 = '-' // Isi Apikey Plta Lu
+global.capikey2 = '-' // Isi Apikey Pltc Lu
+global.domain2 = '-' // Isi Domain Lu
+global.docker2 = "ghcr.io/cekilpedia/vip:sanzubycekil" //jangan di ubah
+global.eggsnya2 = '15' // id eggs yang dipakai
+global.location2 = '1' // id location
+/////////////////////////////////////////////////////////////////////////////////
+global.domainotp = "https://claudeotp.com/api"
+global.apikeyotp = "a395f97fe99f4fad0e790d10af518b9a"
+global.eggsnya = '15' // id eggs yang dipakai
+global.location3 = '1' // id location
+global.tekspushkon = ""
+global.tekspushkonv2 = ""
+global.tekspushkonv3 = ""
+global.tekspushkonv4 = ""
+/////////////////////////////////////////////////////////////////////////////////
+//BATAS//
+/////////////////////////////////////////////////////////////////////////////////
+global.mess = {
+   wait: "*Gausah buru-buru ngentot! Sabar dikit lah!* 🖕",
+   success: "Nih hasilnya nyet, jangan nangis 🖕",
+   on: "Fitur nyala, lu pada bisa seneng sekarang 😒", 
+   off: "Diem-diem aja fiturnya mati 🚫",
+   query: {
+       text: "Dasar bego, text mana anjir? Kaga ngasih argumen 🤬",
+       link: "Link kontol mana? Kaga ngasih link sok mau pake fitur 😤",
+   },
+   error: {
+       fitur: "Bangsat error lagi! Lu yang salah pake kali ngentot! 😡",
+   },
+   only: {
+       group: "Goblok! Ini cuma bisa di grup tolol! 🖕",
+       private: "Anjing! Pake di private aja nyet! 😠",
+       owner: "Lu siapa cong? Owner bukan lu bjir! 😾",
+       admin: "Dasar kacung! Lu bukan admin sialan! 👿",
+       badmin: "Bot gue bukan admin sini ngentod! Belagu lu 😈",
+       premium: "Miskin lu! Bayar dulu ke owner kalo mau pake 🤑",
+   }
+}
+//========================================\\
+global.decor = {
+	menut: '❏═┅═━–〈',
+	menub: '┊•',
+	menub2: '┊',
+	menuf: '┗––––––––––✦',
+	hiasan: '꒦ ͝ ꒷ ͝ ꒦ ͝ ꒷ ͝ ꒦ ͝ ꒷ ͝ ꒦ ͝ ꒷ ͝ ꒦ ͝ ꒷ ͝ ꒦ ͝ ꒷ ͝ ꒦ ͝ ꒷ ͝ ꒦ ͝ ꒷',
+
+	menut: '––––––『',
+    menuh: '』––––––',
+    menub: '┊☃︎ ',
+    menuf: '┗━═┅═━––––––๑\n',
+	menua: '',
+	menus: '☃︎',
+
+	htki: '––––––『',
+	htka: '』––––––',
+	haki: '┅━━━═┅═❏',
+	haka: '❏═┅═━━━┅',
+	lopr: 'Ⓟ',
+	lolm: 'Ⓛ',
+	htjava: '❃'
+}
+
+//===========================//
+
+global.rpg = {
+    emoticon(string) {
+        string = string.toLowerCase()
+        let emot = {
+            level: '📊',
+            limit: '🎫',
+            health: '❤️',
+            exp: '✨',
+            atm: '💳',
+            money: '💰',
+            bank: '🏦',
+            potion: '🥤',
+            diamond: '💎',
+            common: '📦',
+            uncommon: '🛍️',
+            mythic: '🎁',
+            legendary: '🗃️',
+            superior: '💼',
+            pet: '🔖',
+            trash: '🗑',
+            armor: '🥼',
+            sword: '⚔️',
+            makanancentaur: "🥗",
+            makanangriffin: "🥙",
+            makanankyubi: "🍗",
+            makanannaga: "🍖",
+            makananpet: "🥩",
+            makananphonix: "🧀",
+            pickaxe: '⛏️',
+            fishingrod: '🎣',
+            wood: '🪵',
+            rock: '🪨',
+            string: '🕸️',
+            horse: '🐴',
+            cat: '🐱',
+            dog: '🐶',
+            fox: '🦊',
+            robo: '🤖',
+            petfood: '🍖',
+            iron: '⛓️',
+            gold: '🪙',
+            emerald: '❇️',
+            upgrader: '🧰',
+            bibitanggur: '🌱',
+            bibitjeruk: '🌿',
+            bibitapel: '☘️',
+            bibitmangga: '🍀',
+            bibitpisang: '🌴',
+            anggur: '🍇',
+            jeruk: '🍊',
+            apel: '🍎',
+            mangga: '🥭',
+            pisang: '🍌',
+            botol: '🍾',
+            kardus: '📦',
+            kaleng: '🏮',
+            plastik: '📜',
+            gelas: '🧋',
+            chip: '♋',
+            umpan: '🪱',
+            naga: "🐉",
+            phonix: "🦅",
+            kyubi: "🦊",
+            griffin: "🦒",
+            centaur: "🎠",
+            skata: '🧩'
+        }
+        let results = Object.keys(emot).map(v => [v, new RegExp(v, 'gi')]).filter(v => v[1].test(string))
+        if (!results.length) return ''
+        else return emot[results[0][0]]
+    }
+}
+
+//new
+global.prefix = ['','.','!']
+global.sessionName = 'Zynz_Rorw' // Jangan di ubah takut nanti error
+global.hituet = 0
+//media target
+global.thum = fs.readFileSync("./data/image/thumb.jpg") //ur thumb pic
+global.log0 = fs.readFileSync("./data/image/thumb.jpg") //ur logo pic
+global.err4r = fs.readFileSync("./data/image/thumb.jpg") //ur error pic
+global.thumb = fs.readFileSync("./data/image/thumb.jpg") //ur thumb pic
+global.defaultpp = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png?q=60' //default pp wa
+
+//menu image maker
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+
+global.keyopenai = "pk-pIWAlRroXTOAigkWdHcYvmlmgzEQXuoMWbVAaLAVZswSRbEB"
+//documents variants
+global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+global.doc2 = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+global.doc3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+global.doc4 = 'application/zip'
+global.doc5 = 'application/pdf'
+global.doc6 = 'application/vnd.android.package-archive'
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
